8abfe8cd71dbd4ef4300e91473997ad2afb689a3
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <151159508+narendragitala@users.noreply.github.com>
2024-03-08 20:36:06 +0530

    Update leave.py

--- DEADLYSPAM/plugins/leave.py
+++ DEADLYSPAM/plugins/leave.py
@@ -40,4 +40,4 @@ async def _(event):
         else:
             await event.reply(usage, parse_mode=None, link_preview=None)   
     else:
-        await event.reply("**ᴅᴇᴘʟᴏʏ ʏᴏᴜʀ ᴏᴡɴ ᴋɪɴɢ x ᴛᴇᴀᴍ ʙʀᴀɴᴅᴇᴅ-ꜱᴘᴀᴍʙᴏᴛ!**") 
+        await event.reply("**ᴅᴇᴘʟᴏʏ ʏᴏᴜʀ ᴏᴡɴ ɴᴀʀᴜᴊᴀᴀᴛ x ʙᴀʙʏsᴘᴀᴍ ꜱᴘᴀᴍʙᴏᴛ!**") 
