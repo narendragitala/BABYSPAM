f3d8253c74667d2e9b0eab44aa74c5b4044b6afb
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <151159508+narendragitala@users.noreply.github.com>
2024-03-08 20:34:58 +0530

    Update ping.py

--- DEADLYSPAM/plugins/ping.py
+++ DEADLYSPAM/plugins/ping.py
@@ -23,4 +23,4 @@ async def ping(event):
         text = await event.reply(f"» ᴘᴏɴɢ!", parse_mode=None, link_preview=None)
         end = datetime.now()
         result = (end - start).microseconds / 1000
-        await text.edit(f"» ᴋɪɴɢ x ᴛᴇᴀᴍ ʙʀᴀɴᴅᴇᴅ  🔥\n\n💫 ᴀʙ ʙᴏʟ ʙʜᴀɪ ᴋɪsᴋᴇ ᴍᴀᴀ ᴄʜᴏᴅɴᴀ ʜᴀɪ: {result} ms🥀`")
+        await text.edit(f"» ɴᴀʀᴜᴊᴀᴀᴛ x ᴋɪɴɢ ᴏғ ᴛᴇʟᴇɢʀᴀᴍ  🔥\n\n💫 ᴀʙ ʙᴏʟ ʙʜᴀɪ ᴋɪsᴋᴇ ᴍᴀᴀ ᴄʜᴏᴅɴᴀ ʜᴀɪ: {result} ms🥀`")
